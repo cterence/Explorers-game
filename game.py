60bf4d12b3c1f0fdfcb67a6cea052981acefefb9
Térence Chateigné <terence.chateigne@etu.utc.fr>
2019-01-29 14:55:51 +0100

    Changement de la fonction move

--- game.py
+++ game.py
@@ -10,26 +10,31 @@ class Dot:
 		self.alive = True
 		self.score = 0
 		self.moves = []
+	
+	def __str__(self) :
+		return "x : "+str(self.x)+", y : "+str(self.y)+", alive : "+str(self.alive)+", score : "+str(self.score)
 		
 	def isAlive(self) :
 		if self.x < 0 or self.x > 600 or self.y < 0 or self.y > 600 :
 			self.alive = False
 			
 	def move(self) :
-		newX, newY = 0, 0
-		if (random.random() <= 0.5) :
-			newX = self.x+5
+		newX, newY = self.x, self.y
+		rand = random.random()
+		
+		if (rand <= 0.25) :
+			newX+=5
+		elif (rand <= 0.5) :
+			newX-=5
+		elif (rand <= 0.75) :
+			newY+=5
 		else :
-			newX = self.x-5
-		if (random.random() <= 0.5) :
-			newY = self.y+5
-		else :
-			newY = self.y-5
+			newY-=5
 		
 		self.moves.append((newX,newY))
-		print(newX, newY)
 		self.x, self.y = newX, newY
 		self.isAlive()
+	
 
 class Board:
 	def __init__(self) :
@@ -44,25 +49,28 @@ class Board:
 		self.canvas.delete("all")
 		self.canvas.create_oval(300, 50, 305, 55, fill='green')	
 		self.update()
-		self.canvas.after(5, self.displayDots)
+		self.canvas.after(50, self.displayDots)
 	
 	def update(self) :
 		for dot in self.dots :
+			print("Before :"+str(dot))
 			if dot.alive == True :
-				dot.move()	
+				dot.move()
 				self.canvas.create_oval(dot.x, dot.y, dot.x+5, dot.y+5, fill='black')
-			print(dot.x, dot.y)
+				print("After :"+str(dot))
+		print('end')
 		
 if __name__ == '__main__' :
 
 	
 	fenetre = Tk()
-	dot = Dot()
+	dot1 = Dot()
+	dot2 = Dot()
 	
 	board = Board()
-	board.addDot(dot)
-	board.addDot(dot)
-	board.addDot(dot)
+	board.addDot(dot1)
+	board.addDot(dot2)
+	
 	board.canvas.pack()
 	
 	board.displayDots()
